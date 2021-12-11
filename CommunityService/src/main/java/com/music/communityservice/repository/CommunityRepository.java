6cbb662bb7ebeac64747da9fc53e81dabd397377
1952554wucehnwei <1669572385@qq.com>
2021-12-12 02:48:35 +0800

    community后端

---
+++ CommunityService/src/main/java/com/music/communityservice/repository/CommunityRepository.java
@@ -0,0 +1,13 @@
+package com.music.communityservice.repository;
+
+import com.music.communityservice.model.Community;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+// DAO Database Access Object
+@Repository
+public interface CommunityRepository extends JpaRepository<Community,Integer> {
+    public Community findCommunityByMomentId(Integer momentId);
+}
+
